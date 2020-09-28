4cec156cb3d8244f8bba71bf8cdeb50985c27833
William <williamzak0@gmail.com>
2020-09-28 22:56:24 +0000

    rendered info about single urls

--- express_server.js
+++ express_server.js
@@ -25,7 +25,14 @@ app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//render the urls
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 })
+
+//render info about a single url
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls_show', templateVars);
+}) 
